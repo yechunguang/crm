b8816579109e8827ec8c2b9ce7bc8e2642825f9b
yechunguang <812086869@qq.com>
2020-08-28 19:57:52 +0800

    登录完成

---
+++ js/axios.defaults.js
@@ -0,0 +1,32 @@
+axios.defaults.baseURL="http://localhost:8888";
+axios.defaults.headers['Content-Type']='application/x-www-form-urlencoded';//使数据以表单形式提交
+
+axios.defaults.transformRequest=function(data){
+    if(!data) return data;
+    let result='';
+    for(let attr in data){
+        if(! data.hasOwnProperty(attr)) break;
+        result +=`&${attr}=${data[attr]}`
+    }
+    return result.substring(1);
+}
+axios.interceptors.request.use(config=>{
+    return config;
+})
+
+axios.interceptors.response.use(response=>{
+    return response.data;
+}),reason=>{
+    if(reason.response){
+        switch(String(reason.response.status)){
+            case "404":
+            alert("当前请求的地址不存在")
+            break;
+            default:
+                break;
+        }
+        return Promise.reject(reson)
+    }
+}
+
+
---
+++ js/login.js
@@ -0,0 +1,21 @@
+$(function(){
+    //登录功能
+    $(".submit").click(async function(){
+        let account=$(".userName").val().trim();
+        let password=$(".userPass").val().trim();
+
+        if(account ===""||password===""){
+            alert("账户密码不能为空")
+            return//不会向下执行了
+        }
+        password = md5(password)
+         
+         let res=await axios.post("/user/login",{ account,password})
+         if(parseInt(res.code)===0){
+             alert("登录成功")
+             window.location.href="index.html"
+             return
+         }
+         alert("登录失败")
+    })
+})
